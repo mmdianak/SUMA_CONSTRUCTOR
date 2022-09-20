ff222668a9584a0a90737128ca0b322cbb0d8685
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-20 10:22:46 -0600

    Agregar archivos de proyecto.

---
+++ SUMA_CONSTRUCTOR/SUMA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_CONSTRUCTOR
+{
+    internal class SUMA
+    { //campos, propiedades, como es 
+        private float n1, n2;
+        //constructor
+        public SUMA(float n1, float n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public float sumar()
+        {
+            return (n1 + n2);
+        }
+    }
+}
+ 
